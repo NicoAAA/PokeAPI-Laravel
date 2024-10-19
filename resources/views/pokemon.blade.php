28b583d03732baf50080dc92efa5c2e688b3c17f
NicoAAA <nicolas.acosta@soy.sena.edu.co>
2024-10-18 22:35:56 -0500

    Configuracion basica de pokeapi e inicio de css, agregando tipo de letra y shadow a pokemon img

--- resources/views/pokemon.blade.php
+++ resources/views/pokemon.blade.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $pokemon['name'] }}</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-    <h1>{{ ucfirst($pokemon['name']) }}</h1>
-    <img src="{{ $pokemon['sprites']['front_default'] }}" alt="{{ $pokemon['name'] }}">
+    <h1 class= "titulo_de_pokemon">{{ ucfirst($pokemon['name']) }}</h1>
+    <img class= "img_pokemon" src="{{ $pokemon['sprites']['front_default'] }}" alt="{{ $pokemon['name'] }}">
 
     <h2>Stats</h2>
     <ul>
