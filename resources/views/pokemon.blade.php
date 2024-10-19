dfb590f0f9692914fc8c0915883a2134cffea9ca
NicoAAA <nicolas.acosta@soy.sena.edu.co>
2024-10-19 00:52:11 -0500

    CSS implemntado a pokeapi

--- resources/views/pokemon.blade.php
+++ resources/views/pokemon.blade.php
@@ -8,22 +8,35 @@
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 <body>
-    <h1 class= "titulo_de_pokemon">{{ ucfirst($pokemon['name']) }}</h1>
-    <img class= "img_pokemon" src="{{ $pokemon['sprites']['front_default'] }}" alt="{{ $pokemon['name'] }}">
+        <div class="myCard">
+            <div class="innerCard">
+                <div class="frontSide">
+                    <h1 class= "titulo_de_pokem">{{ ucfirst($pokemon['name']) }}</h1>
+                    <img class= "img_pokemon" src="{{ $pokemon['sprites']['front_default'] }}" alt="{{ $pokemon['name'] }}">
+                </div>
+                <div class="backSide">
+                    <div class="stats-habilities-container">
+                        <div class="col">
+                            <p class="title ">Stats</p>
+                            <ul>
+                                @foreach($pokemon['stats'] as $stat)
+                                    <li>{{ $stat['stat']['name'] }}: {{ $stat['base_stat'] }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <p class="title ">Abilities</p>
+                            <ul>
+                                @foreach($pokemon['abilities'] as $ability)
+                                    <li>{{ $ability['ability']['name'] }}</li>
+                                @endforeach
+                            </ul>
+                        </div>
 
-    <h2>Stats</h2>
-    <ul>
-        @foreach($pokemon['stats'] as $stat)
-            <li>{{ $stat['stat']['name'] }}: {{ $stat['base_stat'] }}</li>
-        @endforeach
-    </ul>
-
-    <h2>Abilities</h2>
-    <ul>
-        @foreach($pokemon['abilities'] as $ability)
-            <li>{{ $ability['ability']['name'] }}</li>
-        @endforeach
-    </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
   
 </body>
 </html>
